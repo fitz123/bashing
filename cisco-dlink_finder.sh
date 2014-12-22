d8b447649d5a098523ab4cf0f8cc7aac1f5d1f1a
fitz123 <musor.zdes@gmail.com>
2014-12-22 12:15:59 +0600

    a

--- cisco-dlink_finder.sh
+++ cisco-dlink_finder.sh
@@ -17,11 +17,11 @@ printf "show interfaces status\nshow mac address-table\n" > $defdir/cmdfile
 cat $devip | while read line
     do
      host=`echo $line | cut -d',' -f5 | sed 's/\x0D//g;s/\x08\{3\}//g;s/--More--//g;s/  \+/;/g'`
-        if [ -d "$output=$defdir/temp-mac-tables/" ]; then
-            output=$defdir/temp-mac-tables/host
+        if [ -d "$defdir/temp-mac-tables/" ]; then
+            output=$defdir/temp-mac-tables/$host
         else
             mkdir $defdir/temp-mac-tables
-            output=$defdir/temp-mac-tables/host
+            output=$defdir/temp-mac-tables/$host
         fi
      #Getting credentials from $line
      user="root"
