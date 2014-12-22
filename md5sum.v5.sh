5b8e5a32500a165e88beca3261004388ddf49ae0
fitz123 <musor.zdes@gmail.com>
2014-12-22 20:32:59 +0500

    New upload

---
+++ md5sum.v5.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# 
+# The script about md5 sum multithread calculation
+# Has been used for the fileshare to compare files before and after copy
+#
+result="/home/lugovoy/md5sum.v5.csv"
+log="/home/lugovoy/md5sum.v5.date"
+filelist="/home/lugovoy/filelist.v5"
+listdir="/home/lugovoy/filelists/"
+lists="/home/lugovoy/lists"
+#root="$1"
+root="/media/samba"
+
+rm -f $log
+find $listdir -type f -exec /bin/rm {} \;
+
+echo -e "Find starts"'\t'`date`'\t'`date +%s` >> $log
+echo -e "Find starts"'\t'`date`'\t'`date +%s`
+#find "${root}" -type f ! -path "/media/samba/NAS/*" ! -path "/media/samba/nas/*" ! -path "/media/samba/printer/*" > $filelist
+files=`wc -l $filelist | cut -d" " -f1`
+echo -e "Find finished"'\t'$files" files found"'\t'`date`'\t'`date +%s` >> $log
+echo -e "Find finished"'\t'$files" files found"'\t'`date`'\t'
+if [ ! -d "$listdir" ]; then mkdir $listdir; fi
+cd $listdir
+maxjobs=200
+let splitby=$files/$maxjobs
+#echo $files
+#echo $splitby
+split -l $splitby -a 10 $filelist
+cd ../
+find $listdir -type f > $lists
+
+date_s=`date +%s`
+
+rm -f $result
+
+file_proc () {
+    file=$1
+    sum=`md5sum "${file}" | cut -d" " -f1`
+    fname=//"${file#/media/}"
+    sname="${file#/media/samba/*/}"
+    echo -e $sum'\t'$sname'\t'$fname >> $list.csv
+#    echo -e $sum'\t'$sname'\t'$fname
+}
+
+#parallelize_f () {
+#            jobcnt=(`jobs -p`)
+#            if [ ${#jobcnt[@]} -lt $maxjobs ] ; then
+#               echo ${#jobcnt[@]}
+#                file_proc "$1" &
+#                shift
+#            fi
+#wait
+#}
+
+while read list; do
+        while read file; do file_proc "$file"; done < $list &
+done < $lists
+
+wait
+
+cat $listdir/x*.csv >> $result
+date_f=`date +%s`
+let date_=($date_f-$date_s)
+
+files=`wc -l $result | cut -d" " -f1`
+
+echo -e "Proccess finished"'\t'$files" in "$date_" seconds"'\t'`date`'\t'`date +%s` >> $log
+echo -e "Proccess finished"'\t'$files" in "$date_" seconds"'\t'`date`'\t'`date +%s`
