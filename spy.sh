734b6ed4f54772659d0320a81e9cb27d06db0e60
fitz123 <musor.zdes@gmail.com>
2015-01-04 20:58:59 +0500

    Test is successfull

--- spy.sh
+++ spy.sh
@@ -1,24 +1,41 @@
 #!/bin/bash
-
-# Script periodically uploads to git a file
+#
+# Script periodically uploads to git a $file
+# 
 
 # Set variables
-gdir="/opt/spy"
+gdir=~/spy/
 file="autobrightness-sample.jpg"
 sdir="/tmp"
 commit='date +%Y/%m/%d-%H:%M:%S'
 sleep=$(cat ~/.config/wildguppy/config.json | cut -d\" -f4)
 condition="wildguppy.py"
+repo="https://fitz123@bitbucket.org/fitz123/spy.git"
+pushevery=600
 
+# Check if work directory exist
+[ -d $gdir ] || { mkdir -p $gdir && cd $gdir && git clone $repo; }
 cd $gdir
-git remote set-url origin https://fitz123@bitbucket.org/fitz123/spy.git
-let stime=$sleep+3
+git remote set-url origin $repo
 
-while [ `ps -ef | grep "$condition" | wc -l` -gt 1 ]; do 
-	git --work-tree=$sdir add $file && \
-	git commit -m "`$commit`"
-	ptime=`date +%M | sed 's/^.//'`
-	[ $ptime -eq 0 ] && git push origin master && sleep 60
-	sleep $stime
+# Script starts
+let stime=$sleep+3
+let time1=`date +%s`/$pushevery
+
+# If a process $condition doesn't run - sleep for a 5 mins 
+while true; do
+	
+	# While a process $condition up and running we add and commit our file 
+	while [ `ps -ef | grep "$condition" | wc -l` -gt 1 ]; do 
+		git --work-tree=$sdir add $file && \
+		git commit -m "`$commit`"
+		# Check the time, if more then $pushevery time left - do the push
+		let time2=`date +%s`/$pushevery
+		[ $time1 -ne $time2 ] && git push origin master && time1=$time2
+		sleep $stime
+	done
+
+sleep 300
 done
+
 exit 0
